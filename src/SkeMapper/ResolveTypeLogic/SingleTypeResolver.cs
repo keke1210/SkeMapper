cfa41aafc77a694fcdf30c1b791213e02034abb0
keke <keke@SB-LP>
2021-01-20 22:47:30 +0100

    Added memoization to the recursive function ResolveTypeMap

--- src/SkeMapper/ResolveTypeLogic/SingleTypeResolver.cs
+++ src/SkeMapper/ResolveTypeLogic/SingleTypeResolver.cs
@@ -1,6 +1,8 @@
 ﻿using SkeMapper.Extensions;
 using SkeMapper.TypeContainer;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -9,6 +11,7 @@ namespace SkeMapper.ResolveTypeLogic
     internal class SingleTypeResolver : IResolver
     {
         private readonly IContainer container;
+        private readonly static ConcurrentDictionary<object, object> memo = new ConcurrentDictionary<object, object>();
 
         public SingleTypeResolver(IContainer container)
         {
@@ -22,6 +25,11 @@ namespace SkeMapper.ResolveTypeLogic
 
             if (destinationType == sourceType)
                 return source;
+
+            if (memo.ContainsKey(source))
+            {
+                return memo[source];
+            }
 
             var sourceProperties = sourceType.GetProperties().ToDictionary(k => k.Name.ToLower(), v => v.GetValue(source, null));
             var destinationProperties = destinationType.GetProperties().Select(x => x.Name.ToLower());
@@ -39,7 +47,9 @@ namespace SkeMapper.ResolveTypeLogic
                     {
                         var child = this.ResolveTypeMap(propertyValue);
 
-                        currentProperty.SetValue(destination, child, null);
+                        memo.TryAdd(propertyValue, child);
+
+                        currentProperty.SetValue(destination, memo[propertyValue], null);
                     }
                     else
                     {
